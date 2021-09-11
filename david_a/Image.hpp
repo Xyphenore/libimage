4b37eb6450b177d7e77fa5517e9245a3527d7521
= <=>
2021-09-12 01:13:06 +0200

    Ajout des define de corrections

--- david_a/Image.hpp
+++ david_a/Image.hpp
@@ -1,6 +1,15 @@
 #ifndef IMAGE_HPP
 #define IMAGE_HPP
 
+// Options de correction
+// #define CORR_PGMASCII //P2
+// #define CORR_PPMASCII //P3
+// #define CORR_READCOLORJPEG
+// #define CORR_WRITECOLORJPEG
+// #define CORR_READCOLORTGARLE
+// #define CORR_BRESENHAM
+// #define CORR_TEMPLATE
+
 #include <iostream>
 
 const char *const identifier = "david_a";
