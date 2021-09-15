d1a3fb47085e31ba0cfaf3e70e2bf691101cd841
= <=>
2021-09-16 00:27:33 +0200

    Ajout de test pour fillRectangle et reactangle, simpleScale

--- david_a/essai.cpp
+++ david_a/essai.cpp
@@ -15,32 +15,45 @@ using namespace std;
 */
 #include "Image.hpp"
 
-extern const char * const identifier;   // Ne pas modifier ces deux lignes ! Vos changements au sujet
-extern const char * const informations; // de ceux deux constantes doivent être faits dans Image.cpp.
+extern const char* const identifier;   // Ne pas modifier ces deux lignes ! Vos changements au sujet
+extern const char* const informations; // de ceux deux constantes doivent être faits dans Image.cpp.
 
 
-int main(int argc,char *argv[])
-{
- cout << "Votre identifiant tel que declare dans Image.cpp : " << identifier << endl;
- cout << "Les informations que vous avez decide d'indiquer au correcteur : " << endl << informations << endl;
- try
-  {
-   ifstream ifimage("../ressources/chat.pgm", ios::binary );
+int main( int argc, char* argv[] ) {
+    cout << "Votre identifiant tel que declare dans Image.cpp : " << identifier << endl;
+    cout << "Les informations que vous avez decide d'indiquer au correcteur : " << endl << informations << endl;
+    try {
+        ifstream ifimage( "../ressources/chat.pgm", ios::binary );
 
-   const GrayImage * const gray = GrayImage::readPGM(ifimage);
+        const GrayImage* const gray = GrayImage::readPGM( ifimage );
 
-   ofstream ofimg("../ressources/chatbis.pgm", ios::binary );
-   gray->writePGM(ofimg);
+        GrayImage imgcopy( *gray );
+
+        imgcopy.clear( 15 );
+
+        GrayImage imgrect( *gray );
+
+        GrayImage imgscale( *gray );
+
+        auto pimg = imgscale.simpleScale( 2 * imgscale.getWidth(), 2 * imgscale.getHeight() );
+
+        imgrect.fillRectangle( 150, 120, 10, 20, 0 );
+        imgcopy.fillRectangle( 150, 120, 150, 50, 255 );
+
+        ofstream ofimgrect( "../ressources/chatrect.pgm", ios::binary );
+        imgrect.writePGM( ofimgrect );
+
+        ofstream ofimgclear( "../ressources/chatclear.pgm", ios::binary );
+        imgcopy.writePGM( ofimgclear );
+
+        std::ofstream of( "../ressources/chatscale.pgm", ios::binary );
+        pimg->writePGM( of );
 
 
-
-  } // Trois types d'exceptions seront attrapés (les chaines C et C++ ainsi que
-    // les std::exception et toutes ses dérivées). N'utilisez pas autre chose !
- catch(exception& e)
-  { cerr<< "Exception :"<<e.what()<< endl; }
- catch(string& e)
-  { cerr<< "Exception string :"<<e<< endl; }
- catch(const char * e)
-  { cerr<< "Exception const char* :"<<e<< endl; }
- return 0;
+    } // Trois types d'exceptions seront attrapés (les chaines C et C++ ainsi que
+        // les std::exception et toutes ses dérivées). N'utilisez pas autre chose !
+    catch ( exception& e ) { cerr << "Exception :" << e.what() << endl; } catch ( string& e ) {
+        cerr << "Exception string :" << e << endl;
+    } catch ( const char* e ) { cerr << "Exception const char* :" << e << endl; }
+    return 0;
 }
